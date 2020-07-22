186e3757b2b21725525c64e9cfec477e7392a9a3
BANO.notIT <BANOnotIT@users.noreply.github.com>
2020-07-22 21:55:58 +0300

    Create setup-zsh.sh

---
+++ setup-zsh.sh
@@ -0,0 +1,15 @@
+sudo apt install zsh
+chsh -s /usr/bin/zsh
+
+# installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# installing plugins
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i "s/^plugins=(.*)/plugins=(git zsh-autosuggestions extract zsh-history-substring-search zsh-syntax-highlighting)/g" .zshrc
+
+# installing powelevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g" .zshrc
