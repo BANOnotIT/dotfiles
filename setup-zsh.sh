b9f2ab7ba14dd91460d1a8e0888a18a2821110d0
BANO.notIT <BANOnotIT@users.noreply.github.com>
2020-07-22 21:58:44 +0300

    update quotes

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -12,4 +12,4 @@ sed -i "s/^plugins=(.*)/plugins=(git zsh-autosuggestions extract zsh-history-sub
 
 # installing powelevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g" .zshrc
+sed -i "s/^ZSH_THEME=.*/ZSH_THEME=\x22powerlevel10k/powerlevel10k\x22/g" .zshrc
